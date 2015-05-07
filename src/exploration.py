f0b6299b4563d83550ab7ad65d61d85cdf574dac
davimi <da.mischke@gmail.com>
2015-05-07 20:28:25 +0200

    Data Exploration
    First version of looking at plots etc. What kind of variables to plot
    may be dependent on what we will use in the prediction method in the
    end.

--- src/exploration.py
+++ src/exploration.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 import sys
-
-#for loading smaller dataset
-sys.path.append('C:\Users\david\Dropbox\Data Mining Techniques\Assignment2')
+import pylab
 
 from dataset_filter import sample_dataset
 
@@ -12,7 +11,7 @@ from dataset_filter import sample_dataset
 #ds = pd.read_csv('training_set_VU_DM_2014.csv')
 
 #load a smaller dataset
-ds = sample_dataset('training_set_VU_DM_2014.csv', 'small_dataset.csv')
+ds = sample_dataset(os.path.join('..', 'data', 'training_set_VU_DM_2014.csv'), os.path.join('..', 'data' , 'unbalanced_small_dataset.csv'))
 
 def STATAclean(ds):
     STATAds = ds
@@ -21,28 +20,39 @@ def STATAclean(ds):
     
     STATAds = STATAds.replace(to_replace='NaN', value='.')
     
-    STATAds.to_csv('STATA_small_dataset.csv', sep=',')
+    STATAds.to_csv(os.path.join('..', 'data', 'STATA_small_dataset.csv'), sep=',')
 
     print "New STATA file created"
 
 #############################
+
 #first 5 entries
 ds.head()
 
+#to count missing values:
+#ds['comp1_rate_percent_diff'].isnull().sum()
+
 #filter stuff
 randomfilter = ds[ds['random_bool'] == 1]
-
-#filter is like a subset of the dataset now
-randomfilter['booking_bool'].mean()
-
 orderedfilter = ds[ds['random_bool'] == 0]
-orderedfilter['booking_bool'].mean()
+
+#difference of likelyhood of booking of already ranked queries and random ordered queries
+orderedfilter['booking_bool'].mean() - randomfilter['booking_bool'].mean()
+
+#correlations
+ds[['booking_bool', 'prop_starrating', 'prop_review_score', 'prop_brand_bool', 'prop_location_score1', 'prop_location_score2', 'prop_log_historical_price', 'price_usd', 'promotion_flag', 'srch_length_of_stay', 'srch_saturday_night_bool', 'orig_destination_distance']].corr()
+
+#############
+#plotting
 
 #count proportion of bookings -> need to count the occurences of a particular value before plotting
 ds['booking_bool'].value_counts()
 
-#plot that 
+#booking likelyhood
 ds['booking_bool'].value_counts().plot(kind='pie', figsize=(5,5), labels=['not booked', 'booked'])
+
+#length of stay
+ds[ds['srch_length_of_stay'] < 20]['srch_length_of_stay'].value_counts().plot(kind='pie', figsize=(5,5))
 
 #review scores
 ds['prop_review_score'].value_counts()
@@ -50,9 +60,12 @@ ds['prop_review_score'].value_counts().plot(kind='pie', figsize=(5,5))
 
 #star ratings
 ds['prop_starrating'].value_counts().plot(kind='pie', figsize=(5,5))
-ds['prop_starrating'].plot(kind='hist', bins=5, figsize=(5,5))
 
+#plot of historical price
+ds['prop_log_historical_price'].describe()
+#not sold hotels are counted as 0
+ds[ds['prop_log_historical_price'] != 0]['prop_log_historical_price'].plot(kind='box')
+#save figure with no-whitespace around picture
+pylab.savefig(os.path.join('..', 'figures', 'price_boxplot.png'), bbox_inches='tight')
 
-    
-    
-    
+ds[ds['booking_bool'] == 1][['visitor_hist_adr_usd', 'prop_log_historical_price']].corr()

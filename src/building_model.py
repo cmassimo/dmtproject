1e8678f86cb0425dd0ef383c5962f3d9f8ba6481
doaguilera <dom.aguilera.g@gmail.com>
2015-05-08 13:05:47 +0200

    Model

---
+++ src/building_model.py
@@ -0,0 +1,26 @@
+#Building model
+from sklearn import ensemble
+from sklearn import datasets
+from sklearn.utils import shuffle
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+######################################################################
+# Load data
+boston = datasets.load_boston()
+X, y = shuffle(boston.data, boston.target, random_state=13)
+X = X.astype(np.float32)
+offset = int(X.shape[0] * 0.9)
+X_train, y_train = X[:offset], y[:offset]
+X_test, y_test = X[offset:], y[offset:]
+
+###############################################################################
+# Fit regression model
+params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 1,
+          'learning_rate': 0.01, 'loss': 'ls'}
+clf = ensemble.GradientBoostingRegressor(**params)
+
+clf.fit(X_train, y_train)
+mse = mean_squared_error(y_test, clf.predict(X_test))
+print("MSE: %.4f" % mse)

a9d6ae60fef3cf68e697133d6c77f25321028705
davimi <da.mischke@gmail.com>
2015-05-14 17:33:03 +0200

    float

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,8 +1,9 @@
 from sklearn import preprocessing as pp
+import numpy as np
 import os
 from dataset_filter import *
 
-dataset = pd.read_csv(os.path.join('..', 'data', 'training_set_VU_DM_2014.csv'))
+#dataset = pd.read_csv(os.path.join('..', 'data', 'training_set_VU_DM_2014.csv'))
 
 def feature_extraction(dset):
 
@@ -10,13 +11,13 @@ def feature_extraction(dset):
     field_list = ['promotion_flag', 'srch_length_of_stay', 'srch_booking_window', \
         'srch_adults_count', 'srch_children_count', 'prop_id', 'booking_bool', 'prop_location_score2']
 
-    ds = dset[field_list].astype(float64)
+    ds = dset[field_list].astype(float)
 
-    ds.loc[:, 'prop_review_score'] = dset['prop_review_score'].fillna(0).astype(float64)
-    ds.loc[:, 'loc_ratio2'] = loc_ratio2(dset).fillna(0).astype(float64)
-    ds.loc[:, 'norm_star_rating'] = norm_pcid(dset, 'prop_starrating').astype(float64)
-    ds.loc[:, 'nlog_price'] = log_norm_srch_id(dset, 'price_usd').fillna(0).astype(float64)
-    ds.loc[:, 'label'] = label(dset).astype(float64)
+    ds.loc[:, 'prop_review_score'] = dset['prop_review_score'].fillna(0).astype(float)
+    ds.loc[:, 'loc_ratio2'] = loc_ratio2(dset).fillna(0).astype(float)
+    ds.loc[:, 'norm_star_rating'] = norm_pcid(dset, 'prop_starrating').astype(float)
+    ds.loc[:, 'nlog_price'] = log_norm_srch_id(dset, 'price_usd').fillna(0).astype(float)
+    ds.loc[:, 'label'] = label(dset).astype(float)
 
     return ds
 

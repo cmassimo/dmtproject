47b11c971d4820cea57137af2bc7b962612b07d1
Carlo Maria Massimo <cmassim@gmail.com>
2015-05-14 16:41:08 +0200

    fixed prototype

--- src/prototype.py
+++ src/prototype.py
@@ -1,19 +1,19 @@
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn import ensemble
 from sklearn.utils import shuffle
 from sklearn import cross_validation
+from sklearn.metrics import mean_squared_error
 from data_preparation import *
 
-#osn = pd.read_csv('../data/osn.csv')
-#dataset = get_final_trainingset(osn)
+whole_dataset = pd.read_csv(os.path.join('..', 'data', 'training_set_VU_DM_2014.csv'))
+osn = get_final_trainingset(whole_dataset)
 
-dataset = pd.read_csv('../data/osn.csv')
+x = osn.filter(regex="[^(label)]")
+y = osn.filter(regex="label")
 
-x = dataset.filter(regex="[^(label)]")
-y = dataset.filter(regex="label")
 X, Y = shuffle(x, y, random_state=13)
 
 offset = int(X.shape[0] * 0.9)
@@ -24,5 +24,5 @@ clf = ensemble.GradientBoostingClassifier()
 clf.fit(X_train, y_train)
 prediction = clf.predict(X_test)
 probs = clf.predict_proba(X_test)
-
+mse = mean_squared_error(y_test, prediction)
 
